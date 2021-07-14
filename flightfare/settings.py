0f7a16a8d9bafe79ce70bcf84bc0e28af40073cf
Shivang-Agarwal11 <shivangagarwal11.sa@gmail.com>
2021-07-14 07:51:00 +0000

    flightfare

--- flightfare/settings.py
+++ flightfare/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j@g@!1^5touiv7n$9#vk0@0uii*4s5fc+unwup5qe0$3dmgz@l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['farecalc.pythonanywhere.com']
 
 
 # Application definition
